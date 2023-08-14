dde57b5158dc048d6c51da91333d31162f194c88
Martin Thrömer <m.throemer@gmail.com>
2023-08-15 01:57:33 +0200

    save

--- public/index.php
+++ public/index.php
@@ -7,78 +7,117 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
 </head>
-<body class="bg-success bg-opacity-25">
+<body class="background-clr">
 
 
-<header id="section1" class="text-center">
-    <div class="container bg-success bg-opacity-25 p-0">
+<header class="text-center mb-3">
+    <div class="container background-clr-logo p-0">
 
         <div class="row align-items-center">
-            <div class="col-4">
-                <img src="img/logo.PNG" alt="Logo" class="img-fluid">
+            <div class="col-5 px-4">
+                <img src="img/logo.png" alt="Logo" class="img-fluid w-100">
             </div>
-            <div class="col-8">
-                <h1 class="text-start fs-1">Lilly`s</h1>
-                <h2 class="text-center fs-3">Relax Massage</h2>
+            <div class="col-7">
+                <h1 class="text-start font-snell ps-4 fs-1 mt-3 fw-bolder">Lilly´s</h1>
+                <h2 class="text-center fs-2 fw-light">Relax Massage</h2>
+
+                <h3 class="mt-4 text-start fs-6 fw-lighter">Muskelentspannende und schmerzlindernde Massagen</h3>
             </div>
-        </div>
-
-        <div class="row mt-4">
-            <h3 class="lead fs-2">Muskelentspannende und schmerzlindernde Massagen</h3>
-
         </div>
 </header>
 
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light border-top border-bottom p-3">
-    <div class="container">
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" href="#pricelist">Leistungen und Preise</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#about">Über mich</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#galery">Galerie</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#contact">Kontakt</a>
-            </li>
-        </ul>
+<div class="container p-0">
+    <nav class="navbar navbar-expand-lg background-clr-nav border-top border-bottom p-3">
+        <div class="container">
+            <ul class="navbar-nav">
 
-    </div>
-</nav>
+                <li class="nav-item">
+                    <a class="nav-link" href="#about">Über mich</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#pricelist">Leistungen und Preise</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#booking">Terminbuchung</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#galery">Galerie</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#contact">Kontakt</a>
+                </li>
+            </ul>
+
+        </div>
+    </nav>
+</div>
 
 
-<div class="container bg-light">
+<div class="container">
     <div class="row">
-        <div class="col-auto">
-            <section id="pricelist" class="row py-5">
+        <div class="col">
+
+            <section id="about" class="row mt-3 py-3 background-clr-odd">
                 <div class="container">
-                    <h4 class="mb-5">Leistungen und Preise</h4>
+                    <h4 class="mb-3">Über mich</h4>
+
+                    <div class="row">
+                        <div class="col-4 col-sm-12"></div>
+                        <div class="col-4 col-sm-4">
+                            <img src="img/about.png" class="rounded img-fluid rounded-circle" alt="Bild über mich">
+                        </div>
+
+                        <div class="col-12 col-sm-8">
+                            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+                                Lorem
+                                ipsum dolor sit amet.</p>
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+
+            <section id="pricelist" class="row mt-2 py-3 background-clr-even">
+                <div class="container">
+                    <h4 class="mb-3">Leistungen und Preise</h4>
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+                        Lorem
+                        ipsum dolor sit amet.</p>
+                </div>
+            </section>
+
+            <section id="booking" class="row mt-2 py-3 background-clr-odd">
+                <div class="container">
+                    <h4 class="mb-3">Terminbuchung</h4>
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+                        Lorem
+                        ipsum dolor sit amet.</p>
+                </div>
+            </section>
+
+            <section id="galery" class="row mt-2 py-3 background-clr-even">
+                <div class="container">
+                    <h4 class="mb-3">Galerie</h4>
+
+                    <div class="row g-3">
+                        <div class="col-4 image-zoom">
+                            <img src="img/galery/welcome.jpeg" class="rounded img-fluid" alt="Bild mit einem thailändischen 'Willkommen'">
+                        </div>
+                        <div class="col-4 image-zoom">
+                            <img src="img/galery/massage_table.jpeg" class="rounded img-fluid" alt="Bild mit einem thailändischen 'Willkommen'">
+                        </div>
+                        <div class="col-4 image-zoom">
+                            <img src="img/galery/stones.jpeg" class="rounded img-fluid" alt="Bild mit einem thailändischen 'Willkommen'">
+                        </div>
+                    </div>
 
                 </div>
             </section>
 
-            <section id="about" class="row py-5 bg-light">
+            <section id="contact" class="row mt-2 py-3 background-clr-odd">
                 <div class="container">
-                    <h4>Über mich</h4>
-                    <p>
-                        Lorem Ipsum
-                    </p>
-                </div>
-            </section>
-
-            <section id="galery" class="row py-5">
-                <div class="container">
-                    <h4>Galerie</h4>
-                </div>
-            </section>
-
-            <section id="contact" class="row py-5">
-                <div class="container">
-                    <h4>Kontakt</h4>
+                    <h4 class="mb-3">Kontakt</h4>
                     <p>Öffnungszeiten:</p>
                 </div>
             </section>
@@ -88,8 +127,8 @@
 </div>
 
 
-<footer class="py-4 text-center bg-success bg-opacity-50">
-    <div class="container">
+<footer class="text-center">
+    <div class="container background-clr-logo mt-3 p-2">
         <p>&copy; 2023 <?= (date( 'Y' ) !== '2023' ? ' - ' . date( 'Y' ) : '') ?> Lilly Relax Massage</p>
     </div>
 </footer>
