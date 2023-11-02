35c76d0083067081970a9abe65736a1d215e6380
Martin Thrömer <63214013+meedox-de@users.noreply.github.com>
2023-11-03 00:06:43 +0100

    save

--- public/index.php
+++ public/index.php
@@ -10,20 +10,26 @@
 <body class="background-clr">
 
 
-<header class="text-center mb-3">
+<header class="text-center mb-4">
     <div class="container background-clr-logo p-0">
 
         <div class="row align-items-center">
-            <div class="col-5 px-4">
-                <img src="img/logo.png" alt="Logo" class="img-fluid w-100">
+
+            <div class="col-5 col-md-4 px-4">
+                <a href="/">
+                    <img src="img/logo.png" alt="Logo" class="img-fluid w-100">
+                </a>
             </div>
-            <div class="col-7">
-                <h1 class="text-start font-snell ps-4 fs-1 mt-3 fw-bolder">Lilly´s</h1>
+
+            <div class="col-7 col-md-5">
+
+                <h1 class="text-start font-snell ps-4 fs-1 mt-3 fw-bolder header-font">Lilly´s</h1>
                 <h2 class="text-center fs-2 fw-light">Relax Massage</h2>
 
                 <h3 class="mt-4 text-start fs-6 fw-lighter">Muskelentspannende und schmerzlindernde Massagen</h3>
             </div>
-        </div>
+
+            <div class="col-0 col-md-3"></div>
 </header>
 
 
@@ -32,20 +38,20 @@
         <div class="container">
             <ul class="navbar-nav">
 
-                <li class="nav-item">
+                <li class="nav-item me-3">
                     <a class="nav-link" href="#about">Über mich</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#pricelist">Leistungen und Preise</a>
+                <li class="nav-item me-3">
+                    <a class="nav-link" href="#pricelist">Leistungen & Preise</a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item me-3">
                     <a class="nav-link" href="#booking">Terminbuchung</a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item me-3">
                     <a class="nav-link" href="#galery">Galerie</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#contact">Kontakt</a>
+                <li class="nav-item me-3">
+                    <a class="nav-link" href="#contact">Kontakt & Öffnungszeiten</a>
                 </li>
             </ul>
 
@@ -58,77 +64,208 @@
     <div class="row">
         <div class="col">
 
-            <section id="about" class="row mt-3 py-3 background-clr-odd">
+            <section id="about" class="row mt-4 py-3 background-clr-odd">
                 <div class="container">
-                    <h4 class="mb-3">Über mich</h4>
+                    <h4 class="mb-3 text-center">Über mich</h4>
 
                     <div class="row">
-                        <div class="col-4 col-sm-12"></div>
-                        <div class="col-4 col-sm-4">
-                            <img src="img/about.png" class="rounded img-fluid rounded-circle" alt="Bild über mich">
+                        <div class="col-12 col-md-3">
+                            <div class="position-relative top-50 start-50 translate-middle col-5 col-md-8">
+                                <img src="img/about.png" class="rounded img-fluid rounded-circle" alt="Bild über mich">
+                            </div>
                         </div>
 
-                        <div class="col-12 col-sm-8">
-                            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
-                                Lorem
-                                ipsum dolor sit amet.</p>
+                        <div class="col-12 col-md-9">
+                            <h6>Sawadee kaa</h6>
+                            <p>
+                                Mein Name ist Lilly.
+                                Ich heiße Sie herzlich willkommen bei Lilly Relax & Massage.
+                            </p>
+                            <p>
+                                Gönnen Sie sich eine Auszeit vom stressigen Alltag und genießen Sie eine Massage nach thailändischer Tradition.
+                                In einer entspannten Atmosphäre passe ich meine Massage-Behandlung individuell an Ihre Bedürfnisse an, wobei ich auf meine erlernten Massagetechniken und Erfahrungen vom thailändischen Bildungsministerium zurückgreife.
+                            </p>
+                            <p>Es würde mich sehr freuen, Sie bei Lilly Relax & Massage begrüßen zu dürfen, um gemeinsam in einer entspannten Atmosphäre dem Alltag zu entfliehen und eine wohltuende Massage zu genießen.</p>
                         </div>
 
                     </div>
                 </div>
             </section>
 
-            <section id="pricelist" class="row mt-2 py-3 background-clr-even">
+            <section id="pricelist" class="row mt-4 py-3 background-clr-even">
                 <div class="container">
-                    <h4 class="mb-3">Leistungen und Preise</h4>
-                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
-                        Lorem
-                        ipsum dolor sit amet.</p>
+                    <h4 class="mb-3 text-center">Leistungen und Preise</h4>
+                    <div class="row">
+
+                        <div class="col-12 mt-3 px-5">
+
+                            <h5>Aroma Massage</h5>
+                            <p class="text-muted">Sanfte Massage mit ätherischem Öl stimuliert das Wohlbefinden des Körpers</p>
+                            <div class="row">
+                                <div class="col-1">-</div>
+                                <div class="col-7 border-bottom border-dark-subtle">Rücken, Nacken</div>
+                                <div class="col-4 border-bottom border-dark-subtle text-end">30min / 35€</div>
+                            </div>
+                            <div class="row mt-2">
+                                <div class="col-1">-</div>
+                                <div class="col-7 border-bottom border-dark-subtle">Kopf, Körpermassage</div>
+                                <div class="col-4 border-bottom border-dark-subtle text-end">60min / 50€</div>
+                            </div>
+                            <div class="row mt-2">
+                                <div class="col-1">-</div>
+                                <div class="col-7 border-bottom border-dark-subtle">Körpermassage (Rücken, Nacken, Arme, Beine, Kopf)</div>
+                                <div class="col-4 border-bottom border-dark-subtle text-end">90min / 75€</div>
+                            </div>
+
+
+                            <h5 class="mt-5">Thai Massage</h5>
+                            <p class="text-muted">schmerzlindernde Massage mit Öl und Tigerbalsam</p>
+                            <div class="row">
+                                <div class="col-1">-</div>
+                                <div class="col-7 border-bottom border-dark-subtle">Thai Öl und Balsam (Rücken, Nacken)</div>
+                                <div class="col-4 border-bottom border-dark-subtle text-end">30min / 30€</div>
+                            </div>
+                            <div class="row mt-2">
+                                <div class="col-1">-</div>
+                                <div class="col-7 border-bottom border-dark-subtle">Thai Öl und Balsam (Körpermassage)</div>
+                                <div class="col-4 border-bottom border-dark-subtle text-end">60min / 45€</div>
+                            </div>
+                            <div class="row mt-2">
+                                <div class="col-1">-</div>
+                                <div class="col-7 border-bottom border-dark-subtle">Thai Öl und Balsam (Körpermassage)</div>
+                                <div class="col-4 border-bottom border-dark-subtle text-end">90min / 65€</div>
+                            </div>
+
+
+                            <h5 class="mt-5">Fußreflexzonen Massage</h5>
+                            <p class="text-muted">spezielle Druckpunktmassage an den Fußsohlen, Waden und Oberschenkel, zur Entspannung</p>
+                            <div class="row">
+                                <div class="col-1"></div>
+                                <div class="col-7"></div>
+                                <div class="col-4 border-bottom border-dark-subtle text-end">45min / 30€</div>
+                            </div>
+                            <div class="row mt-2">
+                                <div class="col-1"></div>
+                                <div class="col-7"></div>
+                                <div class="col-4 border-bottom border-dark-subtle text-end">60min / 45€</div>
+                            </div>
+
+
+                            <h5 class="mt-5">Hot Stone Massage</h5>
+                            <p class="text-muted">spezielle Druckpunktmassage an den Fußsohlen, Waden und Oberschenkel, zur Entspannung</p>
+                            <div class="row">
+                                <div class="col-1"></div>
+                                <div class="col-7"></div>
+                                <div class="col-4 border-bottom border-dark-subtle text-end">60min / 60€</div>
+                            </div>
+                            <div class="row mt-2">
+                                <div class="col-1"></div>
+                                <div class="col-7"></div>
+                                <div class="col-4 border-bottom border-dark-subtle text-end">90min / 80€</div>
+                            </div>
+
+                            <h5 class="mt-5">Cellulite Massage</h5>
+                            <p class="text-muted">spezielle Form und Technik der Massage fördert die Durchblutung um das Gewebe zu straffen und die Fettansammlung zu lockern</p>
+                            <div class="row">
+                                <div class="col-1"></div>
+                                <div class="col-7"></div>
+                                <div class="col-4 border-bottom border-dark-subtle text-end">60min / 60€</div>
+                            </div>
+                            <div class="row mt-2">
+                                <div class="col-1"></div>
+                                <div class="col-7"></div>
+                                <div class="col-4 border-bottom border-dark-subtle text-end">90min / 80€</div>
+                            </div>
+
+                        </div>
+                    </div>
                 </div>
             </section>
 
-            <section id="booking" class="row mt-2 py-3 background-clr-odd">
-                <div class="container">
+            <section id="booking" class="row mt-4 py-3 background-clr-odd">
+                <div class="container text-center">
                     <h4 class="mb-3">Terminbuchung</h4>
-                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
-                        Lorem
-                        ipsum dolor sit amet.</p>
+                    <h5>!Bald verfügbar!</h5>
+
+                    <p>
+                        Freuen Sie sich darauf, in Kürze Termine direkt über unsere Website zu buchen. Bis dahin können Sie uns gerne telefonisch unter <a class="text-success" href="tel:+491743870202">(+49) 174 - 387 02 02</a>
+                        kontaktieren.
+                    </p>
                 </div>
             </section>
 
-            <section id="galery" class="row mt-2 py-3 background-clr-even">
+            <section id="galery" class="row mt-4 py-3 background-clr-even">
                 <div class="container">
-                    <h4 class="mb-3">Galerie</h4>
+                    <h4 class="mb-3 text-center">Galerie</h4>
 
                     <div class="row g-3">
                         <div class="col-4 image-zoom">
                             <img src="img/galery/welcome.jpeg" class="rounded img-fluid" alt="Bild mit einem thailändischen 'Willkommen'">
                         </div>
                         <div class="col-4 image-zoom">
-                            <img src="img/galery/massage_table.jpeg" class="rounded img-fluid" alt="Bild mit einem thailändischen 'Willkommen'">
+                            <img src="img/galery/massage_table.jpeg" class="rounded img-fluid" alt="Bild von der Massageliege">
                         </div>
                         <div class="col-4 image-zoom">
-                            <img src="img/galery/stones.jpeg" class="rounded img-fluid" alt="Bild mit einem thailändischen 'Willkommen'">
+                            <img src="img/galery/stones.jpeg" class="rounded img-fluid" alt="Bild mit Steinen für die Hot-Stone Massage">
                         </div>
                     </div>
 
                 </div>
             </section>
 
-            <section id="contact" class="row mt-2 py-3 background-clr-odd">
+            <section id="contact" class="row mt-4 py-3 background-clr-odd">
                 <div class="container">
-                    <h4 class="mb-3">Kontakt</h4>
-                    <p>Öffnungszeiten:</p>
-                </div>
-            </section>
-        </div>
+                    <h4 class="mb-3 text-center">Kontakt</h4>
 
+
+                    <div class="row mt-4">
+                        <div class="container text-center">
+                            <p class="">
+                                Weckenweg 27<br>
+                                85055 Ingolstadt<br>
+                            </p>
+
+                            <a class="btn btn-outline-success" href="tel:+491743870202">(+49) 174 - 387 02 02</a>
+                        </div>
+                    </div>
+
+                    <div class="row mt-5 text-center">
+                        <div class="col-12 col-md-4"></div>
+                        <div class="col-12 col-md-4">
+                            <h6 class="border-bottom border-dark-subtle">Öffnungszeiten:</h6>
+                            <p class="text-muted">gelten nur bei Terminbuchung</p>
+
+                            <div class="row">
+                                <div class="col-3"></div>
+                                <div class="col-3 text-start">Di / Mi / Do</div>
+                                <div class="col-3 text-end">9 - 17 Uhr</div>
+                                <div class="col-3"></div>
+                            </div>
+                            <div class="row">
+                                <div class="col-3"></div>
+                                <div class="col-3 text-start">Fr / Sa</div>
+                                <div class="col-3 text-end">10 - 20 Uhr</div>
+                                <div class="col-3"></div>
+                            </div>
+
+                            <p class="mt-3">
+                                oder Termin nach Vereinbarung
+                            </p>
+                        </div>
+                        <div class="col-12 col-md-4"></div>
+
+                    </div>
+                </div>
+        </div>
+        </section>
     </div>
+
+</div>
 </div>
 
 
 <footer class="text-center">
-    <div class="container background-clr-logo mt-3 p-2">
+    <div class="container background-clr-logo mt-4 p-2">
         <p>&copy; 2023 <?= (date( 'Y' ) !== '2023' ? ' - ' . date( 'Y' ) : '') ?> Lilly Relax Massage</p>
     </div>
 </footer>
